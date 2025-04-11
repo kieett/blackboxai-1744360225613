5b4dc40f8d4febf658c01a765d58cd4b9e8d260b
kieett <22111060604@hunre.edu.vn>
2025-04-11 15:30:33 +0700

    Add lang/en.php 1744360232109

---
+++ lang/en.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'welcome' => 'Welcome to the Adidas Clone',
+    'search_placeholder' => 'Search products...',
+    'add_to_cart' => 'Add to Cart',
+    'out_of_stock' => 'Out of Stock',
+    'product_not_found' => 'Product not found.',
+    // Add more translations as needed
+];
