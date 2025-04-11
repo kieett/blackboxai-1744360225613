12ec2bfcf14e846cc6f575b46c0acf7fc76b05e0
kieett <22111060604@hunre.edu.vn>
2025-04-11 15:30:32 +0700

    Add lang/vi.php 1744360230775

---
+++ lang/vi.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'welcome' => 'Chào mừng bạn đến với Adidas Clone',
+    'search_placeholder' => 'Tìm kiếm sản phẩm...',
+    'add_to_cart' => 'Thêm vào giỏ hàng',
+    'out_of_stock' => 'Hết hàng',
+    'product_not_found' => 'Sản phẩm không tìm thấy.',
+    // Add more translations as needed
+];
